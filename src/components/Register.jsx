d66060117accd56f037355c0b4b2cd850edebdd8
mahizaman490 <mahizaman490@gmail.com>
2023-10-19 01:32:53 +0600

    created the login page with form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,43 @@
 const Register = () => {
     return (
         <div>
-            register page 
+            <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold text-orange-500">Register now!</h1>
+      <p className="py-6 text-orange-400 font-semibold">Connect with our community and our support team to get real-time assistance, tips, and advice from fellow tech enthusiasts.</p>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" placeholder="name" name="name" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn bg-orange-400 text-red-600">Login</button>
+        </div>
+      </form>
+        
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/components/brands/HomeBrandSec.jsx
+++ src/components/brands/HomeBrandSec.jsx
@@ -1,9 +1,10 @@
+import SingleCardH from "../SingleCardH";
 
 const HomeBrandSec = () => {
     return (
         <div  className="w-10/12 mx-auto md:py-40 mt-14 bg-orange-300" >
             <h1 className="text-3xl text-orange-500 font-semibold text-center md:text-6xl">OUR Brands</h1> 
-            
+            <SingleCardH></SingleCardH>
         </div>
     );
 };
