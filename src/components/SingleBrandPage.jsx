215db51c3e3c37bef60f7c90f786c5c3441f1a65
mahizaman490 <mahizaman490@gmail.com>
2023-10-21 19:31:12 +0600

    added a new photo on banner

--- src/components/SingleBrandPage.jsx
+++ src/components/SingleBrandPage.jsx
@@ -34,7 +34,7 @@ const SingleBrandPage = () => {
                         </div>
                     </div>
                     <div id="slide4" className="carousel-item relative w-full">
-                        <img src="/src/images/Home4.jpg" className="w-full" />
+                        <img src="https://i.ibb.co/bmcH8V5/new-smartphone-balancing-with-pebbles.jpg" className="w-full" />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide3" className="btn btn-circle">❮</a>
                             <a href="#slide1" className="btn btn-circle">❯</a>
